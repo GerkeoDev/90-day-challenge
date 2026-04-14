b848ba942de76759f6cefaa2daab06a41f017ad5
GerkeoDev <152994246+GerkeoDev@users.noreply.github.com>
2026-04-14 20:42:52 -0300

    day-07 | ColorBoxes

---
+++ day-07-challenge/page-01/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <Form />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ day-07-challenge/page-01/src/components/Form.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+const Form = () => {
+    const [colorInput, setColorInput] = useState('');
+      const [sizeInput, setSizeInput] = useState('');
+      const [data, setData] = useState([]);
+      const handleChangeSize = (e) => {
+        setSizeInput(e.target.value)
+      }
+      const handleChangeColor = (e) => {
+        setColorInput(e.target.value)
+      }
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        setData([
+          ...data,
+          {
+            color: colorInput,
+            size: parseInt(sizeInput)
+          }
+        ])
+      }
+    
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <h1>Color Boxes</h1>
+                </div>
+                <div className='gridBox'>
+                    <label htmlFor="">Color</label>
+                    <input type="text" onChange={handleChangeColor} value={colorInput}/>
+                </div>
+                <div className='gridBox'>
+                    <label htmlFor="">Size (in pixels)</label>
+                    <input type="number" onChange={handleChangeSize} value={sizeInput}/>
+                </div>
+                <div style={{marginBottom: '10px'}}>
+                    <button type='submit'>Add</button>
+                    <button type='button' onClick={(e)=> console.log(data)}>Log</button>
+                    <button type='button' onClick={(e)=> setData([])}>Clear</button>
+                </div>
+                <div>
+                    {
+                        [...data].reverse().map((box, index) => (
+                            <div key={index} className='flexBox'>
+                                <p style={{backgroundColor: (box.color), height: (box.size), width: (box.size)}}></p>
+                            </div>
+                        ))
+                    }
+                </div>
+            </form>
+        </div>
+    )
+}
+export default Form;
