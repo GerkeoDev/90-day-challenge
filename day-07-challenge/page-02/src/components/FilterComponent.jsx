10a327e2b9414f9297651879f93db93922b6a049
GerkeoDev <152994246+GerkeoDev@users.noreply.github.com>
2026-04-14 22:53:22 -0300

    day-07 | Tabs: v0.2 [Filter]

--- day-07-challenge/page-02/src/components/FilterComponent.jsx
+++ day-07-challenge/page-02/src/components/FilterComponent.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
 const FilterComponent = () => {
+    const urlArray = "https://kitsu.io/api/edge/anime"
+    const [apiData, setApiData] = useState([])
+    const [apiFilter, setApiFilter] = useState(0)
+    useEffect(() => {
+        fetch(urlArray)
+            .then(res=> res.json())
+            .then(data=> setApiData(data.data))
+    },[]);
+    useEffect(() => {
+        apiData.length && console.log(apiData)
+    },[apiData]);
+    const filteredApiData = apiData.filter(object => parseFloat(object.attributes.averageRating)>=apiFilter)
     return (
         <div>
-            Filter Component
+            <div>
+                <p>Filter Component</p>
+                <input type="text" onChange={(e)=>setApiFilter(e.target.value)} value={apiFilter}/>
+                {/* <button onClick={()=> console.log(filteredApiData)}>Log</button> */}
+            </div>
+            {filteredApiData.map((object, index)=> (
+                <div key={index}>
+                    <div>{object.attributes.canonicalTitle}</div>
+                </div>
+            ))}
         </div>
     )
 }
