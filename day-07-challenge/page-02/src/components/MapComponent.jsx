e4e8e2208e7e9c8bfde63254e9b5702ef6585839
GerkeoDev <152994246+GerkeoDev@users.noreply.github.com>
2026-04-15 23:51:27 -0300

    day-08 | Tabs: v0.3 [Map]

--- day-07-challenge/page-02/src/components/MapComponent.jsx
+++ day-07-challenge/page-02/src/components/MapComponent.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 const MapComponent = () => {
+    const urlApi = "https://kitsu.io/api/edge/anime";
+    const [ dataApi, setDataApi ] = useState([])
+    const handleClick = () => {
+        fetch(urlApi)
+            .then(res => res.json())
+            .then(data => {setDataApi(data.data)})
+    }
     return (
         <div>
-            Map Component
+            <div>
+                Map Component
+            </div>
+            <div>
+                <button onClick={handleClick}>Get Animes</button>
+            </div>
+            <div style={{marginTop: "30px"}}>
+                {dataApi.map((element, index) => (
+                    <div key={index}>
+                        <p className="box">
+                            <div>Title: {element.attributes.canonicalTitle}</div>
+                            <div>Description: {element.attributes.description}</div>
+                        </p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
