b5a554dc256542b1eb7e642cd408d1655b3f2794
GerkeoDev <152994246+GerkeoDev@users.noreply.github.com>
2026-04-22 21:37:07 -0300

    Day 15 | Product Manager | Added a delete button to details page

--- day-13-challenge/product-manager/client/src/components/DeleteButton.jsx
+++ day-13-challenge/product-manager/client/src/components/DeleteButton.jsx
@@ -1,6 +1,14 @@
+import axios from "axios";
+const apiUrl = process.env.REACT_APP_API_URL;
 const DeleteButton = props => {
-    const { productId } = props;
-    return <button onClick={() => console.log(productId)}>
+    const { productId, deleteConfirmation } = props;
+    const handleDelete = () => {
+        window.confirm('Are you sure you want to delete this product?') &&
+        axios.delete(`${apiUrl}/${productId}`)
+            .then(res => deleteConfirmation(true))
+            .catch(err => console.log(err));
+    }
+    return <button onClick={handleDelete}>
         Delete
     </button>
 }
--- day-13-challenge/product-manager/client/src/pages/Details.jsx
+++ day-13-challenge/product-manager/client/src/pages/Details.jsx
@@ -1,28 +1,30 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import DeleteButton from "../components/DeleteButton";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 const webUrl = process.env.REACT_APP_URL;
 const Details = () => {
+    const navigate = useNavigate();
     const { id } = useParams();
+    const [deleteConfirmation, setDeleteConfirmation] = useState(false);
     const [product, setProduct] = useState(null);
-    const getProduct = () => {
+    useEffect(() => {
         axios.get(`${apiUrl}/${id}`)
             .then(res => setProduct(res.data))
             .catch(err => console.log(err));
+    }, [id])
+    if (deleteConfirmation) {
+        navigate('/');
     }
-    useEffect(() => {
-        getProduct();
-    }, [])
     return (product && <div>
             <h1>{product.title}</h1>
             <p>Price: {product.price}</p>
             <p>Description: {product.description}</p>
             <div>
                 <span><a href={`${webUrl}/${id}/edit`}>Edit</a> </span>
-                <DeleteButton productId={id} />
+                <DeleteButton productId={id} deleteConfirmation={setDeleteConfirmation} />
             </div>
         </div>
     )
