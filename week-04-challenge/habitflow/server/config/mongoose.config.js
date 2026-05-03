19d5b5ed0ece41759d44c2e4dbf57e7f59af5b4b
GerkeoDev <152994246+GerkeoDev@users.noreply.github.com>
2026-05-02 21:43:32 -0300

    Day 25 | HabitFlow | feat(auth): implement complete backend login system with authentication

---
+++ week-04-challenge/habitflow/server/config/mongoose.config.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/habitflow_db')
+    .then(() => console.log('Connected to the database'))
+    .catch(err => console.log('Error connecting to the database', err))
