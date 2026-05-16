9af66eadef5621df15ddf327255f68ffd308f919
GerkeoDev <152994246+GerkeoDev@users.noreply.github.com>
2026-05-15 22:45:42 -0300

    Day 38 | HabitFlow | refactor: moving to mongodb atlas database

--- week-04-challenge/habitflow/server/config/mongoose.config.js
+++ week-04-challenge/habitflow/server/config/mongoose.config.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI
 
-mongoose.connect('mongodb://localhost/habitflow_db')
+mongoose.connect(MONGODB_URI)
     .then(() => console.log('Connected to the database\n----------------------------------------------------------------'))
     .catch(err => console.log('Error connecting to the database', err))
