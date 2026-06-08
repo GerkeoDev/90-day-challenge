ccb69dff471bf330e7c7fbd81968203dc3bad201
GerkeoDev <152994246+GerkeoDev@users.noreply.github.com>
2026-06-07 21:37:09 -0300

    Week 9 | Master Tailwind | chore: first app commit

---
+++ week-09-challenge/master-tailwind/client/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-xl p-3 text-center">Welcome to Master Tailwind App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
